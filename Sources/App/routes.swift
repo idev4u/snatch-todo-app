ae504e15c1b8665692a44cc4cb6175c4b936e3f2
Norman Sutorius <norman.sutorius@de.ibm.com>
2018-07-10 20:34:33 +0200

    Use a simple filter to query for open tasks

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,11 +1,15 @@
 
 import Routing
 import Vapor
+import Fluent
+
+
 
 /// Register your application's routes here.
 ///
 /// [Learn More →](https://docs.vapor.codes/3.0/getting-started/structure/#routesswift)
 public func routes(_ router: Router) throws {
+    setbuf(stdout, nil)
     // Basic "Hello, world!" example
     router.get("/") { req in
         return "this is the snatch todos service list all todo items use /todos"
@@ -18,27 +22,9 @@ public func routes(_ router: Router) throws {
     router.put("/todos/task/check-off", use: todoController.update)
     router.delete("/todos/task/delete/", Todo.parameter, use: todoController.delete)
     
-    router.get("/todos/taks/list/open") { req -> Future<String> in
-        return req.withPooledConnection(to: .psql) { conn in
-            return conn.query("select task from todos where status = false;").map(to: String.self) { rows in
-                let logger = try req.make(Logger.self)
-                logger.info("Logger created!")
-                var result = [String]()
-                for row in rows {
-                    logger.info("\(row)")
-                    result.append(try row.firstValue(forColumn: "task")?.decode(String.self) ?? "n/a")
-                }
-                return result.description
-            }
-        }
-    }
-    
-    router.get("psql-version") { req -> Future<String> in
-        return req.withPooledConnection(to: .psql) { conn in
-            return conn.query("select version() as v;").map(to: String.self) { rows in
-                return try rows[0].firstValue(forColumn: "v")?.decode(String.self) ?? "n/a"
-            }
+    router.get("/todos/done") { req in
+        return req.withNewConnection(to: .psql) { db -> Future<[Todo]> in
+            return Todo.query(on: req).filter(\Todo.status == true).all()
         }
     }
 }
-
