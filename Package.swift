f6a26a101cc7be171f4f9e6625daadfa2d8c2d04
Norman Sutorius <norman.sutorius@de.ibm.com>
2018-07-10 20:29:48 +0200

    Bump the vapor version

--- Package.swift
+++ Package.swift
@@ -5,10 +5,10 @@ let package = Package(
     name: "snatch-todos",
     dependencies: [
         // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.1"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.4"),
 
         // 🔵 Swift ORM (queries, models, relations, etc) built on PostgreSQL.
-        .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0-rc")
+        .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0-rc.2.3")
     ],
     targets: [
         .target(name: "App",
